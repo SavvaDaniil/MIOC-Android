57825725bd7937d2e9b418fd6b3013bd1fa0255f
SavvaDaniil <savvadaniil25@gmail.com>
2022-01-29 19:19:04 +0300

    Init1

--- app/src/main/java/com/ds/miocnative/config/YoutubePlayerAPIConfig.java
+++ app/src/main/java/com/ds/miocnative/config/YoutubePlayerAPIConfig.java
@@ -2,7 +2,7 @@ package com.ds.miocnative.config;
 
 public class YoutubePlayerAPIConfig {
 
-    private static final String API_KEY = "AIzaSyDon-AWOsi-oVDqfJ8T8XfdPdcW87aqXHE";
+    private static final String API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXX";
 
     public static String getApiKey(){
         return API_KEY;
